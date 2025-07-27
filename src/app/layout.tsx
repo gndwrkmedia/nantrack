62bda710b57ff2558901007c3a94d238fbe7e662
Groundwork Sound Co <groundworksoundco@gmail.com>
2025-07-27 12:59:23 +0000

    can we use better fonts than the fonts we are currently using? please?!?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,12 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
-import { PT_Sans } from 'next/font/google'
+import { Inter } from 'next/font/google'
 import { cn } from '@/lib/utils';
 
-const ptSans = PT_Sans({
+const inter = Inter({
   subsets: ['latin'],
-  weight: ['400', '700'],
-  variable: '--font-pt-sans',
+  variable: '--font-inter',
 })
 
 export const metadata: Metadata = {
@@ -23,12 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
-      </head>
-      <body className={cn("font-body antialiased", ptSans.variable)}>
+      <body className={cn("font-body antialiased", inter.variable)}>
         {children}
         <Toaster />
       </body>
