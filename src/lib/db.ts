b8e3cf2aa4993f0357de02ab2a765f3f53553dc1
Groundwork Sound Co <groundworksoundco@gmail.com>
2025-07-27 15:52:11 +0000

    {"message":"An internal server error occurred during database setup."}

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,16 +1,8 @@
 
 import postgres from 'postgres';
 
-let sql: postgres.Sql;
-
-if (process.env.NODE_ENV === 'production') {
-  sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
-} else {
-  // Ensure the global object has a `sql` property in development
-  if (!(global as any).sql) {
-    (global as any).sql = postgres(process.env.POSTGRES_URL!);
-  }
-  sql = (global as any).sql;
-}
+const sql = postgres(process.env.POSTGRES_URL!, {
+  ssl: 'require',
+});
 
 export default sql;
